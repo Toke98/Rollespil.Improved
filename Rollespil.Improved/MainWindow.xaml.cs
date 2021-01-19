6e022d9935c7ccaf24db1ecf0b9d078a45c7b61b
Toke98 <73280669+Toke98@users.noreply.github.com>
2021-01-19 15:00:02 +0100

    Update

--- Rollespil.Improved/MainWindow.xaml.cs
+++ Rollespil.Improved/MainWindow.xaml.cs
@@ -20,9 +20,258 @@ namespace Rollespil.Improved
     /// </summary>
     public partial class MainWindow : Window
     {
+        ///THE MOST IMPORTENT PIECE OF CODE///
+        public static int counter = 0;
+        public static bool outOfTheApp = false;
+        public static bool DeadOrNot = false;
+
+        //Technecal stuff
+        public static ConsoleKeyInfo Exit;
+        public static Random rng = new Random();
+        public static string makeShiftString;
+
+        //Totallaly usless mockery
+        public static int idiotCounter;
+
+        //Character
+
+        public static double ProficiencyBonus = 0;
+        public static double Health = 100;
+        public static double MaxHealth = 100;
+        public static double YourHitDice = 0;
+        public static double SpellcastingAbility = 0;
+        public static double SpellSacveDC = 0;
+        public static double spellAttackBonus = 0;
+
+        public static int Level = 1;
+        public static int numberOfSpells = 0;
+        public static int NumberOfCantrips = 0;
+        public static int newSpells;
+        public static int Initiative;
+        public static int spellSlots;
+        public static int trueSpellSlot;
+        public static int WalkingSpeed;
+        public static int AC;
+        public static int SpellsKnown;
+        public static int SpellCounter;
+        public static int SpellUsed;
+        public static int change = 0;
+
+        public static string yourClass = "";
+        public static string yesOrNo;
+        public static string yourName;
+        public static string MagicalClass;
+        public static string yourRace;
+
+        //Die and chi stuff
+        public static int numberOfSides;
+        public static int dieResult;
+        public static int numberOfRolls;
+        public static int totalDieResult;
+
+        public static bool advantageOrNot = false;
+        public static bool disadvantageOrNot = false;
+
+        ///Uniques
+        //Barbarian
+        public static bool barbarianOrNot = false;
+        public static int rages;
+
+        //Sorcerer
+        public static bool SorcererOrNot = false;
+        public static int SorcererPoints;
+
+        //Warlock
+        public static bool warlockOrNot = false;
+        public static bool[] Arcadium = { false, false, false, false };
+
+
+        //Combat
+        public static bool combatOrNot = false;
+        public static bool consentrationOrNot = false;
+
+        public static int DamageDone = 0;
+        public static int DeathDie;
+        public static int LifeDie;
+        public static int DeathCounter;
+        public static int roundCounter;
+
+        public static double DeathThreasHold = 0;
+
+
+
+        //Spells
+        public static int[] levelSpellSlots = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public static int[] trueSpellslots = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public static int consentrationChech;
+
+        public static string YourSpells;
+        public static string[] allSpellLevels = { "1'st Level Spells: ", "2'nd Level Spells: ", "3rd Level Spells: ", "4'th Level Spells: ", "5'th Level Spells: ", "6'th Level Spells: ", "7'th Level Spells: ", "8'th Level Spells: ", "9'th Level Spells: " };
+
+        public static bool levelUpSpells = false;
+
+        public static List<string> Spells = new List<string>();
+
+
+        ///Weapons
+        public static string searchTool;
+
+        //Your weapon
+        public static int yourWeapon = -1;
+        public static int weaponDamageDealt;
+        public static int toHit;
+        public static int attackRoll;
+
+        public static bool weaponPro = false;
+        public static bool rangedWeapon = false;
+
+
+        //Classes and Races
+        public static string[] Classess = { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard" };
+        public static double[] HitDice = { 12, 8, 8, 8, 10, 8, 10, 10, 8, 6, 8, 6 };
+        public static string[] Races = { "Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling" };
+
+        public static bool SpeelsOrNot = false;
+        public static bool SpellTotal = false;
+
+        public static double[] SavingThrows = { 0, 0, 0, 0, 0, 0 };
+        public static bool[] deathSavingProfieciency = { false, false, false, false, false, false };
+
+
+        //Stats and Skills
+        public static string[] AllThings = { "Strength: ", "Dexterity: ", "Constitition: ", "Intelligence: ", "Wisdom: ", "Charisma: " };
+        public static int[] StatValue = { 0, 0, 0, 0, 0, 0 };
+
+        public static string Str = "Athletics: ";
+        public static double[] StrMods = { 0 };
+        public static bool[] strProOrNot = { false };
+
+        public static string[] Dex = { "Acrobatics: ", "Sleight of hand: ", "Stealth: " };
+        public static double[] DexMods = { 0, 0, 0 };
+        public static bool[] dexProOrNot = { false, false, false };
+
+        public static string[] Int = { "Arcana: ", "History: ", "Investigation: ", "Nature: ", "Religion: " };
+        public static double[] IntMods = { 0, 0, 0, 0, 0 };
+        public static bool[] IntProOrNot = { false, false, false, false, false };
+
+        public static string[] Wis = { "Animal Handling: ", "Insight: ", "Medicine: ", "Perception: ", "Survival: " };
+        public static double[] WisMods = { 0, 0, 0, 0, 0 };
+        public static bool[] wisProOrNot = { false, false, false, false, false };
+
+        public static string[] Cha = { "Deception: ", "Intimidation: ", "Perfermance: ", "Persuasion: " };
+        public static double[] ChaMods = { 0, 0, 0, 0 };
+        public static bool[] chaProOrNot = { false, false, false, false };
+
+
+        //Modifiers
+        public static double[] Modifier = { 0, 0, 0, 0, 0, 0 };
+
+        public static int atributes = 0;
+
+
+        ///Rando
+        // A-Z
+        public static string[] AZ = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z" };
+
+        //a-z
+        public static string[] az = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z" };
+
+        //Stats
+        public static string RandoName;
+        public static string firstLetter;
+        public static string letter;
+        public static string restName;
+        public static string firstName;
+        public static string lastName;
+
+        public static int nameLetters;
+
         public MainWindow()
         {
             InitializeComponent();
+            spell();
+            dndclasses();
+        }
+
+        //Spells in Combobox
+        public void spell()
+        {
+            //Cantrips * Combobox
+            {
+                Spell_0.ItemsSource = SpellList.Cantrips;
+                Spell_0.SelectedIndex = 0;
+            }
+
+            //Spell 1 * Combobox
+            {
+                Spell_1.ItemsSource = SpellList.Level1Spells;
+                Spell_1.SelectedIndex = 0; 
+            }
+
+            //Spell 2 * Combobox
+            {
+                Spell_2.ItemsSource = SpellList.Level2Spells;
+                Spell_2.SelectedIndex = 0;
+            }
+            
+            //Spell 3 * Combobox
+            {
+                Spell_3.ItemsSource = SpellList.Level3Spells;
+                Spell_3.SelectedIndex = 0;
+            }
+            
+            //Spell 4  Combobox
+            {
+                Spell_4.ItemsSource = SpellList.Level4Spells;
+                Spell_4.SelectedIndex = 0;
+            }
+            
+            //Spell 5  Combobox
+            {
+                Spell_5.ItemsSource = SpellList.Level5Spells;
+                Spell_5.SelectedIndex = 0;
+            }
+
+            //Spell 6  Combobox
+            {
+                Spell_6.ItemsSource = SpellList.Level6Spells;
+                Spell_6.SelectedIndex = 0;
+            }
+
+            //Spell 7  Combobox
+            {
+                Spell_7.ItemsSource = SpellList.Level7Spells;
+                Spell_7.SelectedIndex = 0;
+            }
+
+            //Spell 8  Combobox
+            {
+                Spell_8.ItemsSource = SpellList.Level8Spells;
+                Spell_8.SelectedIndex = 0;
+            }
+
+            //Spell 9  Combobox
+            {
+                Spell_9.ItemsSource = SpellList.Level9Spells;
+                Spell_9.SelectedIndex = 0;
+            }
+        }
+
+        //Classes in Combobox
+        public void dndclasses()
+        {
+            DropClass.ItemsSource = Classess;
+            DropClass.SelectedIndex = 0;
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            DropClass.Visibility = Visibility.Hidden;
+            LabelClass.Content = DropClass.SelectedItem;
+            TextBoxName.Visibility = Visibility.Hidden;
+            LabelName.Content = TextBoxName.Text;
+
+            
         }
     }
 }
---
+++ Rollespil.Improved/SpellList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rollespil.Improved
+{
+    class SpellList
+    {
+        //Skills
+        public static string[] Skills = { "Acrobatics: ", "Animal Handling: ", "Arcana: ", "Athletics: ", "Deception: ", "History: ", "Insight: ", "Intimidation: ", "Investigation: ", "Medicine: ", "Nature: ", "Perception: ", "Perfermance: ", "Persuasion: ", "Religion: ", "Sleight of hand: ", "Stealth: ", "Survival: " };
+
+        // Cantrips (35)
+        public static string[] Cantrips = { "Acid Splash", "Blade Ward", "Chill Touch", "Control Flames", "Create Bonfire", "Dancing Lights", "Druidcraft", "Eldrith Blast", "Fire Bolt", "Friends", "Frostbite", "Guidance", "Gust", "Light", "Mage Hand", "Magic Stone", "Mending", "Message", "Minor Illusion", "Mold Earth", "Poison Spray", "Prestidigitation", "Produce Flame", "Ray of Frost", "Resistance", "Sacred Flame", "Shape Water", "Shillelagh", "Shocking Grasp", "Spare the Dying", "Thaumaturgy", "Thorn Whip", "Thunderclap", "True Strike", "Vicious Mockery" };
+
+        // 1'st level spells (66)
+        public static string[] Level1Spells = { "Absorb Elements", "Alarm", "Animal Friendship", "Armor of Agathys", "Arms of Hadar", "Bane", "Beast Bond", "Bless", "Burning Hands", "Catapult", "Charm Person", "Chromatic Orb", "Color Spray", "Command", "Compelled Duel", "Comprehend Languages", "Create or Destroy Water", "Cure Wounds", "Detect Evil And Good", "Detect Magic", "Detect Poison and Disease", "Disguise Self", "Dissonant Whispers", "Divine Favor", "Earth Tremor", "Ensnaring Strike", "Entangle", "Expeditious Retreat", "Faerie Fire", "False Life", "Feather Fall", "Find Familiar", "Fog Cloud", "Goodberry", "Grease", "Guiding Bolt", "Hail of Thorns", "Healing Word", "Hellish Rebuke", "Heroism", "Hex", "Hunter's Mark", "Ice Knife", "Identify", "Illusory Script", "Inflict Wounds", "Jump", "Longstrider", "Mage Amor", "Magic Missile", "Protection from Evil and Good", "Purify Food and Drink", "Ray of Sickness", "Sanctuary", "Searing Smite", "Shield", "Shield of Faith", "Silent Image", "Sleep", "Speak with Animals", "Tasha's Hideous Laughter", "Tenser's Floating Disk", "Thunderous Smite", "Thunderwave", "Unseen Servant", "Witch Bolt", "Wrathful Smite" };
+
+        // 2'nd level spells (67)
+        public static string[] Level2Spells = { "Aganazzar's Scorcher", "Aid", "Alter Self", "Animal Messenger", "Arcane Lock", "Augury", "Barkskin", "Beast Sence", "Blindness/Deafness", "Blur", "Branding Smite", "Calm Emotions", "Cloud of Daggers", "Continual Flame", "Cordan of Arrows", "Crown of Madness", "Darkness", "Darkvision", "Detect Thoughts", "Dust Devil", "Earthbind", "Enhance Ability", "Enlarge/Reduce", "Entrhall", "Find Steed", "Find Traps", "Flame Blade", "Flaming Sphere", "Gentle Repose", "Gust of Wind", "Heat Metal", "Hold Person", "Invisibility", "Knock", "Lesser Restoration", "Levitate", "Locate Animals or Plants", "Locate Object", "Magic Mouth", "Magic Weapon", "Maximilian's Earthen Grasp", "Melf's Acid Arrow", "Mirror Image", "Misty Step", "Moonbeam", "Nystul's Magic Aura", "Pass Without Trace", "Phantasmal Force", "Prayer of Healing", "Protection from Poison", "Pyrotechnics", "Ray of Enfeeblement", "Rope Trick", "Scorching Ray", "See Invisibility", "Shatter", "Silence", "Skywrite", "Snilloc's Snowball Swarm", "Spider Climb", "Spike Growth", "Spiritual Weapon", "Suggestion", "Warding Bond", "Warding Wind", "Web", "Zone of Truth" };
+
+        // 3'rd level spells (56)
+        public static string[] Level3Spells = { "Animate Dead", "Aura of Vitality", "Beacon of Hope", "Bestow Curse", "Blinding Smite", "Blink", "Call Lightning", "Clairvoyance", "Conjure Animals", "Conjure Barrage", "Counterspell", "Create Food and Water", "Crusader's Mantle", "Daylight", "Dispel Magic", "Elemental Weapon", "Erupting Eath", "Fear", "Feign Death", "Fireball", "Flame Arrows", "Fly", "Gaseous Form", "Glyph of Warding", "Haste", "Hunger of Hadar", "Hypnotic Pattern", "Leomund's Tiny Hut", "Lightning Arrow", "Lightning Bolt", "Magic Circle", "Major Image", "Mass Healing Word", "Meld Into Stone", "Melf's Minute Meteors", "Nondetection", "Phantom Steed", "Plant Growth", "Protection from Energy", "Remove Curse", "Revivify", "Sending", "Sleet Storm", "Slow", "Speak with Dead", "Speak with Plants", "Spirit Guardians", "Stinking Cloud", "Tidal Wave", "Tongues", "Vampiric Touch", "Wall of Sand", "Wall of Water", "Water Breathing", "Water Walk", "Wind Wall" };
+
+        // 4'th level spells (39)
+        public static string[] Level4Spells = { "Arcane Eye", "Aura of Life", "Aura of Purity", "Banishment", "Blight", "Compulsion", "Confusion", "Conjure Minor Elementals", "Conjure Woodland Beings", "Control Water", "Death Ward", "Dimension Door", "Divination", "Dominate Beast", "Elemental Bane", "Evard's Black Tentacales", "Fabricate", "Fire Shield", "Freedom of Movement", "Giant Insect", "Grasping Vine", "Greater Invisibility", "Guardian of Faith", "Hallucinatory Terrain", "Ice Storm", "Leomund's Secret Chest", "Locate Creature", "Mordenkainen's Faithful Hound", "Mordenkainen's Private Sanctum", "Otiluke's Resilient Sphere", "Phantasmal Killer", "Polymorph", "Staggering Smite", "Stone Shape", "Stoneskin", "Strom Sphere", "Vitriolic Sphere", "Wall of Fire", "Watery Sphere" };
+
+        // 5'th level spells (46)
+        public static string[] Level5Spells = { "Animate Object", "Antilife Shell", "Awaken", "Banishing Smite", "Bigby's Hand", "Circle of Power", "Cloudkill", "Commune", "Commune with Nature", "Cone of Cold", "Conjure Elemental", "Conjure Volley", "Contact Other Plane", "Contagion", "Control Winds", "Creation", "Destructive Wave", "Dispel Evil amd Good", "Dominate Person", "Dream", "Flame Strike", "Geas", "Greater Restoration", "Hallow", "Hold Monster", "Immolation", "Insect Plague", "Legend Lore", "Maelstrom", "Mass Curse Wounds", "Mislead", "Modify Memory", "Passwall", "Planar Binding", "Raise Dead", "Rary's Telepathic Bond", "Reincarnate", "Scrying", "Seeming", "Swift Quiver", "Telekinesis", "Teleportation Circle", "Transmute Rock", "Tree Stride", "Wall of Force", "Wall of Stone" };
+
+        // 6'th level spells (37)
+        public static string[] Level6Spells = { "Arcane Gate", "Blade Barrier", "Bones of the Earth", "Chain Lightning", "Circle of Death", "Conjure Fey", "Contingency", "Create Undead", "Disintegrate", "Drawmij's Instant Summon", "Eyebite", "Find Path", "Flesh to Stone", "Forbiddance", "Globe of Invulnerability", "Harm", "Heal", "Heroes' Feast", "Investiture of Flame", "Investiture of Ice", "Investiture of Stone", "Investiture of Wind", "Magic Jar", "Mass Suggestion", "Move Earth", "Otiluke's Freezing Sphere", "Otto's Irresistible Dance", "Planar Ally", "Primordial Ward", "Programmed Illusion", "Sunbeam", "Transport via Plants", "True Seeing", "Wall of Ice", "Wall of Thorns", "Wind Walk", "Word of Recall" };
+
+        // 7'th level spells (21)
+        public static string[] Level7Spells = { "Conjure Celestial", "Delayed Blast Fireball", "Divine Word", "Etherealness", "Finger of Death", "Fire Storm", "Forcecage", "Mirage Arcane", "Mordenkainen's MagnificentMansion", "Mordenkainen's Sword", "Plane Shift", "Prismatic Spray", "Project Image", "Regenerate", "Resurrection", "Reverse Gravity", "Sequester", "Simulacrum", "Symbol", "Teleport", "Whirlwind" };
+
+        // 8'th level spells (19)
+        public static string[] Level8Spells = { "Abi-Dalzim's Horrid Wilting", "Animal Shapes", "Antimagic Field", "Antipathy/Sympathy", "Clone", "Control Weather", "Demiplane", "Dominate Monster", "Earthquake", "Feeblemind", "Glibness", "Holy Aura", "Incendiary Cloud", "Maze", "Mind Blank", "Power Word Stund", "Sunburst", "Telepathy", "Tsunami" };
+
+        // 9'th level spells (15)
+        public static string[] Level9Spells = { "Astral Projection", "Foresight", "Gate", "Imprisonment", "Mass Heal", "Power Word Heal", "Power Word Kill", "Prismatic Wall", "Shapechange", "Storm of Vengeance", "Time Stop", "True Polymorph", "True Resurrection", "Weird", "Wish" };
+
+    }
+}
